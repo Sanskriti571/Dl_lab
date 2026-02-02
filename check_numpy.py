21aaba42e99c86a91caa8fcbff18afcbab1972ff
Sanskriti <asanskriti93@gmail.com>
2026-02-02 10:34:25 +0530

    initial commit

---
+++ check_numpy.py
@@ -0,0 +1,6 @@
+# print("DL Environment Ready")
+import numpy as np
+w = np.array([0.2,0.4]) #weights
+x = np.array([1.0,2.0]) #input features 
+print("Weighted sum:",np.dot(x,w))  #dot product of x and w
+
